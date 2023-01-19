f6d46fb49d484fc94b2f633a6f320faa6779a972
jerry.zhou <jerry.zhou@advantest.com>
2023-01-19 17:21:12 +0800

    OpenVC-01 Update UT home page tips.

--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -14,7 +14,7 @@ describe('test/app/controller/home.test.js', () => {
   it('should GET /', async () => {
     return app.httpRequest()
       .get('/')
-      .expect('hi, egg')
+      .expect('Welcome to OpenVC Software Center.')
       .expect(200);
   });
 });
